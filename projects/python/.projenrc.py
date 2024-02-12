52294c7e4899df2a9bf921b8a09fbbb4213e6bdf
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-12 20:53:20 +0900

    save

---
+++ projects/python/.projenrc.py
@@ -0,0 +1,11 @@
+from projen.python import PythonProject
+
+project = PythonProject(
+    author_email="9404411+gomesuit@users.noreply.github.com",
+    author_name="gomesuit",
+    module_name="python",
+    name="python",
+    version="0.1.0",
+)
+
+project.synth()
